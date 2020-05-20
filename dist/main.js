866b6ba51cb4da4d12eee7f900cc49269d7e8b11
Aleexxen <49920406+Aleexxen@users.noreply.github.com>
2020-05-20 06:30:32 +0300

    first commit

---
+++ dist/main.js
@@ -0,0 +1,125 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/*! exports provided: home */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"home\", function() { return home; });\nfunction home() {\r\n    let template = '<h3>Лабораторная работа №4</h3> </br><h4>Выполнила:студентка группы М3306, Алексеева Ксения</h4>'\r\n    app.innerHTML = template\r\n}\n\n//# sourceURL=webpack:///./src/home.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _pokemon__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./pokemon */ \"./src/pokemon.js\");\n\r\n\r\n\r\nlet btnHome = document.querySelector('.home'),\r\n    btnPokemon = document.querySelector('.pokemon'),\r\n    app = document.querySelector('#app')\r\n\r\nObject(_home__WEBPACK_IMPORTED_MODULE_0__[\"home\"])()\r\nbtnHome.onclick = _home__WEBPACK_IMPORTED_MODULE_0__[\"home\"]\r\nbtnPokemon.onclick = _pokemon__WEBPACK_IMPORTED_MODULE_1__[\"pokemon\"]\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/pokemon.js":
+/*!************************!*\
+  !*** ./src/pokemon.js ***!
+  \************************/
+/*! exports provided: pokemon */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"pokemon\", function() { return pokemon; });\nasync function pokemon() {\r\n    let data = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')\r\n    let jsonValue = await data.json()\r\n\r\n\r\n    function pokemonInfo(value) {\r\n        let template = ''\r\n        template += `<img style=\"width: 300px\" src=\"${value.sprites.front_default}\">`\r\n        template += `<h3>${value.name}<h3/>`\r\n        template += `\r\n        <h5>Abilities</h5>\r\n        <ul>\r\n        `\r\n        value.abilities.map(item => {\r\n            template += `<li>${item.ability.name}</li>`\r\n        })\r\n\r\n        template += '</ul>'\r\n\r\n        app.innerHTML = template\r\n    }\r\n\r\n    pokemonInfo(jsonValue)\r\n}\n\n//# sourceURL=webpack:///./src/pokemon.js?");
+
+/***/ })
+
+/******/ });
---
+++ src/home.js
@@ -0,0 +1,4 @@
+export function home() {
+    let template = '<h3>Лабораторная работа №4</h3> </br><h4>Выполнила:студентка группы М3306, Алексеева Ксения</h4>'
+    app.innerHTML = template
+}
